2a5ce60fb55e1611d5e4c8f68343cd9f6287e793
Cade Li <cade.li@qq.com>
2025-09-09 22:40:23 +0800

    feat(*): add codegen and server module

---
+++ codegen/src/test/java/com/github/cadecode/uniboot/codegen/CodegenApplicationTests.java
@@ -0,0 +1,31 @@
+package com.github.cadecode.uniboot.codegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 代码生成器测试类
+ *
+ * @author Cade Li
+ * @since 2024/4/25
+ */
+@SpringBootTest
+public class CodegenApplicationTests {
+
+    /**
+     * 生成代码
+     */
+    @Test
+    public void genCode() {
+        System.out.println(11);
+    }
+
+    /**
+     * 生成数据库文档
+     */
+    @Test
+    public void genDbDoc() {
+
+    }
+
+}
